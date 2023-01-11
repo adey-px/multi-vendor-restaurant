9d6a35901cbe3aa16e489e9d4a8db3bed83c8d05
adey-px <aosteph@gmail.com>
2023-01-10 21:01:00 -0600

    Home page and configured static setting

--- multiVendorAgency/settings.py
+++ multiVendorAgency/settings.py
@@ -27,9 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'multiVendorAgency/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
